ea9ac9c0c1f2c785e731799ddd4c46c9d5078819
Luis Perez <luis1villala@gmail.com>
2017-07-24 07:28:58 -0430

    Se agrega las siguientes actualizaciones:
    1. Los mensajes de la aplicación son traidos desde un .propertie el cual tiene todos los mensajes.
    2. La ruta del WSDL se encuentra en un .propiertie que tiene la ruta a donde deben apuntar el proyecto de paginas webs.
    3. Se agrego la funcionalidad para que se agregaran usuarios a la lista restrictiva.
    4. La data se encuentra configurada en dos archivos .txt, el primero es el UserNamesSaved.txt en donde se encuentran los nombres de usuarios guardados. La otra se llama UserNamesRestrictive.txt en donde se encuentra los nombres de usuario para la lista restrictiva. Este .txt es modificado cada vez que se agrega un nuevo nombre a la lista.
    5. Se modifico la manera como se realiza la generación de la sugerencia. Ahora se realiza a través de un Random por cada petición y además se valida que la sugerencia creada no se encuentre entre la lista restrictiva ni tampoco se haya agregado previamente a la lista de sugerencia actual.
    6. Se realizaron cambios menores a la vista xhml para que los mensajes de error y exito aparezcan en un dialog y que es mas intuitivo.

--- ProyectoFinal_Luis_Perez/Archivos Fuentes/WebPage/src/main/java/com/interctec/java/webpage/controller/UserController.java
+++ ProyectoFinal_Luis_Perez/Archivos Fuentes/WebPage/src/main/java/com/interctec/java/webpage/controller/UserController.java
@@ -20,46 +20,55 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "UserController")
 @SessionScoped
 public class UserController {
+
     private String usuario;
     private RespuestaDTO respuestaDTO;
     private boolean mostrarAlertaGenerica = Boolean.FALSE;
     private String mensajeAlertaGenerica;
     private ResourceBundle i18n = ResourceBundle.getBundle("i18n");
-    
+    private final int MIN_LENGHT_USER_NAME = 6;
+
     @EJB
     private UserDAO userDAO;
-    
-    public void validarUsuarioAction(){
+
+    public void validarUsuarioAction() {
         setMostrarAlertaGenerica(Boolean.TRUE);
-        respuestaDTO = userDAO.validarUserName(usuario);
-        
-        
-        //EL USUARIO ES VALIDO
-        if(respuestaDTO.getValid()){
-            setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.valido"));
-        //EL USUARIO ESTA RESTRINGIDO    
-        }else if(respuestaDTO.getRestricted()){
-            setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.no.restringido"));
-        //EL USUARIO ES INVLAIDO PERO NO ESTA RESTRINGIDO    
-        }else  {
-            setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.en.uso"));
-        }
-    }
-    
-    public void agregarUsuarioRestringidoAction(){
-        respuestaDTO = userDAO.agregarUsuarioRestringido(usuario);
-        setMostrarAlertaGenerica(Boolean.TRUE);
-        if(respuestaDTO.getCodigoRespuestaOperacion().equals(Constants.CODIGO_RESPUESTA_EXITOSO)){
-            if(respuestaDTO.getUserNameToAddExist()){
-                setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.ya.agregado.en.lista.restringidos"));
-            }else {
-                setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.agregado.con.exito"));
+        if (usuario.length() < MIN_LENGHT_USER_NAME) {
+            setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.minlenght"));
+        } else {
+            respuestaDTO = userDAO.validarUserName(usuario);
+
+            //EL USUARIO ES VALIDO
+            if (respuestaDTO.getValid()) {
+                setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.valido"));
+                //EL USUARIO ESTA RESTRINGIDO    
+            } else if (respuestaDTO.getRestricted()) {
+                setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.no.restringido"));
+                //EL USUARIO ES INVLAIDO PERO NO ESTA RESTRINGIDO    
+            } else {
+                setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.en.uso"));
             }
-        }else {
-            setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.no.pudo.ser.agregado"));
         }
     }
-    
+
+    public void agregarUsuarioRestringidoAction() {
+        setMostrarAlertaGenerica(Boolean.TRUE);
+        if (usuario.length() < MIN_LENGHT_USER_NAME) {
+            setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.minlenght"));
+        } else {
+            respuestaDTO = userDAO.agregarUsuarioRestringido(usuario);
+            if (respuestaDTO.getCodigoRespuestaOperacion().equals(Constants.CODIGO_RESPUESTA_EXITOSO)) {
+                if (respuestaDTO.getUserNameToAddExist()) {
+                    setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.ya.agregado.en.lista.restringidos"));
+                } else {
+                    setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.agregado.con.exito"));
+                }
+            } else {
+                setMensajeAlertaGenerica(i18n.getString("common.message.nombre.usuario.no.pudo.ser.agregado"));
+            }
+        }
+    }
+
     public void doCerrarDialogoGenericoCommandButtonAction() {
         setMostrarAlertaGenerica(Boolean.FALSE);
         setMensajeAlertaGenerica("");
